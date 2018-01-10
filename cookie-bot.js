12caf25d7e23aa9c11018f6c6aa2fbcfd4c6f576
riobe <jeremy.pridemore@gmail.com>
2018-01-09 20:14:19 -0700

    Switch to regexes for command/gift recognition and clean up code that performs it.

--- cookie-bot.js
+++ cookie-bot.js
@@ -1,12 +1,27 @@
 'use strict';
 
+const inspect = require('util').inspect;
 const Discord = require('discord.js');
-
 const cookies = require('./lib/cookies');
 
+// 2 capture groups:
+// match[1]: The command itself.
+// match[2]: The rest of the string after the command and any whitespace following it.
+const COMMAND_REGEX = /^\s*!(\w+)\s*(.*)/;
+
+// 3 capture groups:
+// match[1]: A potential negative sign if the user was trying to steal cookies.
+// match[2]: The number of cookies, if specified. Assume 1 if not.
+// match[3]: The mentions to give these cookies to.
+const GIFT_REGEX = /🍪\s*(?:[*xX]\s*(\-?)(\d+))?((?:\s*<@\d+>)+)[^🍪]*/g;
+
+// 1 capture group:
+// match[1]: The ID of the user mentioned.
+const MENTION_REGEX = /<@(\w+)>/g;
 const client = new Discord.Client();
 
-client.login('Mzk4NjI2OTY4NDg2MTUwMTQ0.DTBR6Q.ilrbbDRXzbgeLHy8W6PebtA0qEs');
+const token = process.env.BOT_TOKEN;
+client.login(token);
 
 client.on('ready', () => {
   console.log('Running.');
@@ -29,18 +44,50 @@ process.on('SIGINT', function() {
 client.on('message', message => {
   const content = message.content.trim();
 
-  // One single character check to skip all the rest of the command checks.
-  if (content[0] === '!') {
-    if (content.startsWith('!bake')) {
-      cookies.bakeCookies(message);
-    } else if (content.startsWith('!cookiesof')) {
-      cookies.listCookies(message);
-    } else if (content.startsWith('!cookiehelp')) {
-      cookies.cookieHelp(message);
-    } else if (content.startsWith('!cookies')) {
-      cookies.listOwnCookies(message);
+  let match;
+  if (match = COMMAND_REGEX.exec(content)) {
+    const command = match[1];
+    console.log(`"!${command}" for (${message.author.id}|${message.author.username})`);
+    switch (command) {
+      case 'bake':
+        cookies.bakeCookies(message);
+        return;
+      case 'cookiesof':
+        cookies.listCookies(message);
+        return;
+      case 'cookie':
+      case 'cookies':
+        cookies.listOwnCookies(message);
+        return;
+      case 'cookiehelp':
+      case 'cookieshelp':
+        cookies.cookieHelp(message);
+        return;
     }
-  } else if (content.startsWith(cookies.A_COOKIE)) {
-    cookies.giveCookies(message);
+  }
+
+  const gifts = [];
+  while (match = GIFT_REGEX.exec(content)) {
+    const mentions = [];
+    let mentionMatch;
+    while (mentionMatch = MENTION_REGEX.exec(match[3])) {
+      // Need both ID and user name to make a user.
+      const mention = message.mentions.users.get(mentionMatch[1]);
+      mentions.push({
+        id: mention.id,
+        username: mention.username
+      });
+    }
+
+    gifts.push({
+      // If negative, give 1 cookies, otherwise use the number given if present, otherwise 1.
+      amount: match[1] ? 1 : Number(match[2]) || 1,
+      to: mentions
+    });
+  }
+
+  if (gifts.length) {
+    console.log('Giving cookie gifts: ', inspect(gifts, { depth: 3 }));
+    return cookies.giveCookies(message, gifts);
   }
 });
--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/**
+ * User model. Requires id and username.
+ */
 function User(newUser) {
   if (!newUser.id) {
     throw new Error('Cannot create a user without an ID!');
@@ -12,6 +15,12 @@ function User(newUser) {
   this.id = newUser.id;
   this.username = newUser.username;
   this.mention = `<@${newUser.id}>`;
+
+  console.log('I exist now!', JSON.stringify({
+    id: newUser.id,
+    username: newUser.username,
+    mention: `<@${newUser.id}>`
+  }, null, 2));
 }
 
 module.exports = User;
